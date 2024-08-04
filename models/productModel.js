31c7629ac791bd275ef3ae2079dbb6ccc4954c74
ZIDANE Abdellatif <zidane@et.esiea.fr>
2024-08-04 12:52:13 +0200

    first database version

--- models/productModel.js
+++ models/productModel.js
@@ -1,29 +1,29 @@
 // models/productModel.js
-const db = require('../database')
+const db = require('../database');
 
-const createProduct = (name, price, callback) => {
+const createProduct = (name, price, bare_code, quantity, min_quantity, callback) => {
   db.run(
-    'INSERT INTO product (name, price) VALUES (?, ?)',
-    [name, price],
+    'INSERT INTO product (name, price, bare_code, quantity, min_quantity) VALUES (?, ?, ?, ?, ?)',
+    [name, price, bare_code, quantity, min_quantity],
     function (err) {
       if (err) {
-        return callback(err)
+        return callback(err);
       }
-      callback(null, { id: this.lastID })
-    },
-  )
-}
+      callback(null, { id: this.lastID });
+    }
+  );
+};
 
 const getProducts = (callback) => {
   db.all('SELECT * FROM product', [], (err, rows) => {
     if (err) {
-      return callback(err)
+      return callback(err);
     }
-    callback(null, rows)
-  })
-}
+    callback(null, rows);
+  });
+};
 
 module.exports = {
   createProduct,
   getProducts,
-}
+};
